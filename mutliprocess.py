18837a69fc6fde5ea111af560655defbe9cece0b
gsm <gsm@gsms-MacBook-Pro.local>
2020-05-10 20:17:45 +0800

    多进程

---
+++ mutliprocess.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File  : mutliprocess.py
+# Author: gsm
+# Date  : 2020/5/10
+from multiprocessing import Process, Pool
+import os, time, random
+
+
+def run_proc(name):
+    print('Run child process %s (%s)...' % (name, os.getpid()))
+
+
+def test_process():
+    print('Parent process %s.' % os.getpid())
+    p = Process(target=run_proc, args=('test',))
+    print('Child process will start.')
+    p.start()
+    p.join()
+    print('Child process end.')
+
+
+def long_time_task(name):
+    print('Run task %s (%s)...' % (name, os.getpid()))
+    start = time.time()
+    time.sleep(random.random() * 3)
+    end = time.time()
+    print('Task %s runs %0.2f seconds.' % (name, (end - start)))
+
+
+def test_process_pool():
+    print('Parent process %s.' % os.getpid())
+    p = Pool(4)
+    for i in range(5):
+        p.apply_async(long_time_task, args=(i,))
+    print('Waiting for all subprocesses done...')
+    p.close()
+    p.join()
+    print('All subprocesses done.')
+
+
+if __name__ == '__main__':
+    test_process()
+    test_process_pool()
